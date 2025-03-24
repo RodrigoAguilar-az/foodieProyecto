e571ebbb12d979d03c7ac42fa18f1efdb7f2915f
Juan Monroy <juan.monroy@catolica.edu.sv>
2025-03-24 13:48:47 -0600

    controller y vista de pedidos

---
+++ foodieProyecto/Controllers/PedidosController.cs
@@ -0,0 +1,46 @@
+﻿using foodieProyecto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace foodieProyecto.Controllers
+{
+    public class PedidosController : Controller
+    {
+        private readonly RestauranteContext _context;
+
+        public PedidosController(RestauranteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string estado, string orden, string busqueda)
+        {
+            var pedidos = _context.PedidoLocals.Include(p => p.DetallePedidos).AsQueryable();
+
+            // Filtro por estado
+            if (!string.IsNullOrEmpty(estado))
+            {
+                pedidos = pedidos.Where(p => p.Estado == estado);
+            }
+
+            // Búsqueda por número de mesa
+            if (!string.IsNullOrWhiteSpace(busqueda))
+            {
+                pedidos = pedidos.Where(p => p.IdMesa.ToString().Contains(busqueda));
+            }
+
+            // Ordenar según la selección
+            switch (orden)
+            {
+                case "Por mesa":
+                    pedidos = pedidos.OrderBy(p => p.IdMesa);
+                    break;
+                case "Por estado":
+                    pedidos = pedidos.OrderBy(p => p.Estado);
+                    break;
+            }
+
+            return View(await pedidos.ToListAsync());
+        }
+    }
+}
--- foodieProyecto/Models/RestauranteContext.cs
+++ foodieProyecto/Models/RestauranteContext.cs
@@ -59,7 +59,7 @@ public partial class RestauranteContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=CALEC;Database=restaurante;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-TCPUJ1K; Initial Catalog=restaurante; User ID=juan_monroy; Password=12345; TrustServerCertificate=True; Encrypt=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

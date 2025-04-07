d22bd300484805996c9cf2d54ec774d0cc10f673
Rod <oscar.aguilar1@catolica.edu.sv>
2025-04-07 10:57:21 -0600

    detallitos

---
+++ foodieProyecto/Models/DividirCuentaViewModel.cs
@@ -0,0 +1,34 @@
+﻿using static foodieProyecto.Controllers.PedidosController;
+using System.ComponentModel.DataAnnotations;
+
+namespace foodieProyecto.Models
+{
+    public class DividirCuentaViewModel
+    {
+        public PedidoLocal Pedido { get; set; }
+        public List<ItemSeleccionadoViewModel> ItemsSeleccionados { get; set; }
+
+        [Required(ErrorMessage = "El nombre del cliente es obligatorio")]
+        [Display(Name = "Nombre del Cliente")]
+        public string NombreCliente { get; set; }
+
+        [Required(ErrorMessage = "Seleccione un método de pago")]
+        [Display(Name = "Método de Pago")]
+        public int MetodoPagoId { get; set; }
+
+        // Campos para pago con tarjeta
+        [Display(Name = "Últimos 4 dígitos")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "Debe ingresar exactamente 4 dígitos")]
+        public string Digitos { get; set; }
+
+        [Display(Name = "Titular")]
+        public string Titular { get; set; }
+
+        [Display(Name = "Referencia")]
+        public string Referencia { get; set; }
+
+        public decimal TotalParcial => ItemsSeleccionados?.Sum(i => i.Subtotal) ?? 0;
+        public decimal IVA => TotalParcial * 0.13m;
+        public decimal Total => TotalParcial + IVA;
+    }
+}
---
+++ foodieProyecto/Models/ItemSeleccionadoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace foodieProyecto.Models
+{
+    public class ItemSeleccionadoViewModel
+    {
+        public int DetallePedidoId { get; set; }
+        public string NombreItem { get; set; }
+        public int CantidadTotal { get; set; }
+        public int CantidadSeleccionada { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public bool TieneDescuento { get; set; }
+        public decimal Subtotal => PrecioUnitario * CantidadSeleccionada;
+    }
+}
